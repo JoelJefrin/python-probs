04e4cc4b88e0fa8b1f36417c0264299a01bc7a31
Joel Jefrin <joeljefrin0@gmail.com>
2025-03-29 11:20:42 +0530

    added sentence tokenization

--- words.py
+++ words.py
@@ -4,7 +4,10 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 nltk.download('punkt')
 nltk.download('punkt_tab')
 
-text = 'my project is based on natural language processing'
+text = 'my project is based on natural language processing, may name is joel'
 
 words = word_tokenize(text)
 print("all words: ", words)
+
+sentence = sent_tokenize(text)
+print("the sentance tokenization for this input will be", sentence)
